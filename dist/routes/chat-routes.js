af210542e5dd642d873e7ed95fdaeb2b9b309879
Anand P S <111217709+Anand-p-s@users.noreply.github.com>
2024-08-03 09:42:06 +0530

    Update chat-routes.js

--- dist/routes/chat-routes.js
+++ dist/routes/chat-routes.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../utils/token-managers.js";
 import { deleteAllChats, generateChat, sendAllChats, } from "../controllers/chat-controllers.js";
 const chatRoutes = Router();
 chatRoutes.post("/new", validate(messageValidator), verifyToken, generateChat);
-chatRoutes.get("/all-chats", sendAllChats);
+chatRoutes.get("/all-chats", verifyToken, sendAllChats);
 chatRoutes.delete("/delete-chats", verifyToken, deleteAllChats);
 export default chatRoutes;
 //# sourceMappingURL=chat-routes.js.map
